1f7266dbe8b233390f81f8dccfa6d4dbbc72c2a8
Tiles <stefan@thetilesgame.com>
2025-08-26 22:38:03 +0100

    Fix

--- scripts/10_bootstrap_system.sh
+++ scripts/10_bootstrap_system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-sudo apt-get update -y
-sudo apt-get install -y git python3-venv python3-dev build-essential \
+apt-get update -y
+apt-get install -y git python3-venv python3-dev build-essential \
   libsndfile1 libmpg123-0 ffmpeg tmux curl jq
 nvidia-smi || true
